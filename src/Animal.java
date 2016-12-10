d81a2a87b54d89702e653c7a1a1369a71fd0eb14
Ricardo Lopez <richierich254@gmail.com>
2016-12-09 21:30:36 -0600

    Added examples of Method overloading, type casting, random number generation, getters, setters, etc.

--- src/Animal.java
+++ src/Animal.java
@@ -19,11 +19,110 @@ public class Animal {
 
     protected static int numberOfAnimals = 0;
 
-    static Scanner userinput = new Scanner(System.in);
+    static Scanner userInput = new Scanner(System.in);
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isHasOwner() {
+        return hasOwner;
+    }
+
+    public void setHasOwner(boolean hasOwner) {
+        this.hasOwner = hasOwner;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public long getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(long uniqueID) {
+        this.uniqueID = uniqueID;
+        System.out.println("Unique ID set to: " + this.uniqueID);
+    }
+
+    //example of method overloading
+    public void setUniqueID() {
+        long minNumber = 1;
+        long maxNumber = 1000000;
+        //random number generator and example of type casting.
+        this.uniqueID = minNumber + (long) (Math.random() * ((maxNumber - minNumber) + 1));
+        System.out.println("Unique ID set to: " + this.uniqueID);
+
+        /*
+        more examples of type casting. Long to string.
+        String stringNumber = Long.toString(maxNumber);
+        int numberString = Integer.parseInt(stringNumber);
+        */
+    }
+
+    public char getFavoriteChar() {
+        return favoriteChar;
+    }
+
+    public void setFavoriteChar(char favoriteChar) {
+        this.favoriteChar = favoriteChar;
+    }
+
+    public void setFavoriteChar() {
+        //Generating a random letter character
+        int randomNumber = (int) (Math.random() * 126 + 1);
+        this.favoriteChar = (char) randomNumber;
+
+        //if else example
+        if(randomNumber == 32){
+            System.out.println(" Favorite character set to Space");
+        } else if (randomNumber == 10){
+            System.out.println(" Favorite character set to Newline");
+        } else {
+            System.out.println(" Favorite character set to " + this.favoriteChar);
+        }
+
+        //logical operator examples
+
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
 
     public Animal() {
         numberOfAnimals++;
 
+        //basic mathematical operations
         int sumOfNumbers = 3 + 1;
         System.out.println("3 + 1 = " + sumOfNumbers);
 
@@ -38,6 +137,18 @@ public class Animal {
 
         int modOfNumbers = 3 % 1;
         System.out.println("3 % 1 = " + modOfNumbers);
+
+        //user prompt
+        System.out.print("Enter the name : \n");
+
+        //user input example
+        if(userInput.hasNextLine()){
+
+            this.setName(userInput.nextLine());
+        }
+
+        this.setFavoriteChar();
+        this.setUniqueID();
     }
 
     public static void main(String[] args){
