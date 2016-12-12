0a236c8300bab5acfbf751c7e3dcd9d9f0a07cce
Ricardo Lopez <richierich254@gmail.com>
2016-12-12 16:54:50 -0600

    Added example of reversing the words in a string with string buffer

--- src/Cat.java
+++ src/Cat.java
@@ -19,9 +19,21 @@ public class Cat extends Animal {
 
         String[] strArray = str.split(" ");
 
+        //print original string
+        System.out.println("Original String:  " + str);
+
+        //reverse words in the string.
         for(int i = strArray.length - 1; i >= 0; i--)
             System.out.print(strArray[i] + " ");
-        System.out.println();
+        System.out.println("\nThe words in the string above were reversed\nThe words in the next example will be the original " +
+                "string with the correct words\nprinted in order, but with the letters reversed in each.");
+        StringBuffer sbf = new StringBuffer();
+        for (int i = 0; i < strArray.length; i++){
+            sbf.append(strArray[i]);
+            System.out.print(sbf.reverse() + " ");
+            sbf.delete(0, sbf.length());
+        }
+
 
         Animal[] theAnimals = new Animal[10];
 
